7a70a1ced88569f881c0c232e6b62d17a63216db
Abdullah Ibrahim Dar <ai0064125@gmail.com>
2024-10-13 14:15:25 +0530

    database query

--- nexapp/forms.py
+++ nexapp/forms.py
@@ -7,3 +7,11 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class CollegeSearchform(forms.Form):
+    institute_name = forms.CharField(required=False, max_length=255)
+    city = forms.CharField(required=False, max_length=100, label='City')
+    state = forms.CharField(required=False, max_length=100, label='State')
+    course = forms.CharField(required=False, max_length=100, label='Course')
+    govt = forms.BooleanField(required=False, label='Government Institution')
+
--- nexapp/urls.py
+++ nexapp/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('login/', views.user_login, name='login'),
     path('signup/', views.signup, name='signup'),
+    path('search/', views.search_colleges, name='search_colleges')
 ]
 
