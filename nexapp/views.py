f714097b8f889927d157033efd8f27372a5d7a2d
Abdullah Ibrahim Dar <ai0064125@gmail.com>
2024-10-13 14:16:33 +0530

    database query

--- nexapp/views.py
+++ nexapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
-from .forms import CreateUserForm
+from .forms import CreateUserForm, CollegeSearchform
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from .models import College
+
 
 # Create your views here.
 def home(request):
@@ -30,3 +32,32 @@ def signup(request):
     context = {'form': form}
     return render(request, 'nexapp/signup.html',context=context)
 
+def search_colleges(request):
+    query_result = None
+    if request.method == 'POST':
+        form = CollegeSearchform(request.POST)
+        if form.is_valid():
+            institute_name = form.cleaned_data['institute_name']
+            city = form.cleaned_data['city']
+            state = form.cleaned_data['state']
+            course = form.cleaned_data['course']
+            govt = form.cleaned_data['govt']
+
+            query = College.objects.all()
+
+            if institute_name:
+                query = query.filter(institute_name__icontains=institute_name)
+            if city:
+                query = query.filter(city__icontains=city)
+            if state:
+                query = query.filter(state__icontains=state)
+            if course:
+                query = query.filter(course__icontains=course)
+            if govt is not None:
+                query = query.filter(govt=govt)
+
+            query_result = query
+    else:
+        form = CollegeSearchform()
+        
+    return render(request, 'nexapp/college.html', {'form':form, 'query_result':query_result})
